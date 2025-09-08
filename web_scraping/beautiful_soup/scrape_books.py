c0d4e421ca5ce4ef5de3530bedb908584907f211
Sebastian Montoya <sebastianmontoya@Caterpie.local>
2025-09-09 02:19:27 +1000

    created the beginnings of a web scraper that gets all the book titles from the website

--- web_scraping/beautiful_soup/scrape_books.py
+++ web_scraping/beautiful_soup/scrape_books.py
@@ -5,8 +5,28 @@ import requests
 import smtplib
 
 url = "https://books.toscrape.com/"
-response = requests.get(url)
-text = response.text
 
-soup = BeautifulSoup(text, "html.parser")
-print(soup.prettify())
+def scrape_book_titles(url):
+    page_url = "catalogue/page-1.html"
+
+    # Iterating over the pages.
+    num_pages = 0
+    while page_url:
+        response = requests.get(url + page_url)
+        text = response.text
+        soup = BeautifulSoup(text, "html.parser")
+
+        a_tags = soup.find_all("a")
+        for a_tag in a_tags:
+            print(a_tag.get("title"))
+
+        next_link = soup.find("a", href=True, string="next")
+        if next_link:
+            page_url = next_link["href"]
+        else:
+            page_url = None
+
+
+
+if __name__ == "__main__":
+    scrape_book_titles(url)
